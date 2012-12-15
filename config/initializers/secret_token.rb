851ebee10d60576c33198b55bf8a25c015e440bf
liebeslenchen <lr878249@fh-muenster.de>
2012-12-15 09:56:17 +0100

    Initialisierung von leeze

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leeze::Application.config.secret_token = '42cbd484361a6ec4920f34daaeacdd678cc63bd6df2613105b214d7f67f354962541d9d3a4afc9c5e914ab4a5b371101b1ce2e1f1f4f478f8251229993f27465'
